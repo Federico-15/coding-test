a97750a52ec3ebf7097618bf87ac15098dd3cc8b
류승환 <121941036+Federico-15@users.noreply.github.com>
2025-10-23 13:36:47 +0900

    [D2] Title: 달팽이 숫자, Time: 110 ms, Memory: 27,776 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
@@ -1,53 +1,58 @@
 import java.util.Scanner;
-import java.io.FileInputStream;
+import java.util.*;
 
+// SWEA -> Solution
+// BOJ -> Main
 class Solution
 {
+    
 	public static void main(String args[]) throws Exception
 	{
-        
+
         Scanner sc = new Scanner(System.in);
-		int T;
-		T=sc.nextInt();
-		
-		for(int test_case = 1; test_case <= T; test_case++)
+        int T = sc.nextInt();
+
+        for(int test_case = 1; test_case <= T; test_case++)
 		{
-			int n = sc.nextInt();
-            int[][] arr = new int[n][n];
-            
-            // 방향: 오른쪽, 아래, 왼쪽, 위
-            int[] dx = {0, 1, 0, -1};
-            int[] dy = {1, 0, -1, 0};
-            int dir = 0;
-            
-            int x = 0, y =0;
+            int N = sc.nextInt();
+
+            int[][] arr = new int[N][N];
+
             int num = 1;
+
+            int x = 0;
+            int y = 0;
+
+            int[] dx = {0,1,0,-1};
+            int[] dy = {1,0,-1,0};
+
             
-            while(num<=n*n) {
-            arr[x][y] = num;
-            num++;
-                
-            int nx = x + dx[dir];
-            int ny = y + dy[dir];
-            
-            if (nx < 0 || nx >= n || ny <0 || ny >=n || arr[nx][ny] != 0)
-            {
-            	dir = (dir + 1) % 4;
-                nx = x + dx[dir];
-                ny = y + dy[dir];
+            int dir = 0;
+
+            while(num <= N*N) {
+                arr[x][y] = num;
+                num++;
+
+                int nx = x + dx[dir];
+                int ny = y + dy[dir];
+
+                if(nx < 0 || nx >= N || ny < 0 || ny >=N || arr[nx][ny] != 0){
+                    dir = (dir+1) % 4;
+                    nx = x + dx[dir];
+                    ny = y + dy[dir];
+                }
+
+                x = nx;
+                y = ny;
             }
-                
-            x = nx;
-            y = ny;
+
+            System.out.println("#" + test_case);
+            for(int i = 0; i < N; i++){
+                for(int j = 0; j < N; j++){
+                    System.out.print(arr[i][j]+ " ");
+                }
+                System.out.println();
             }
-            
-			System.out.println("#" + test_case);
-            for(int i = 0; i <n; i ++){
-                for(int j =0; j <n; j++){
-                    System.out.print(arr[i][j] + " ");
-		}
-            System.out.println(" ");
-		}
-		}
-}
+        }
+    }
 }
