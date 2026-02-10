43b821aedb267862703c44c3b5061db5ec9bf2d8
류승환 <121941036+Federico-15@users.noreply.github.com>
2026-02-10 18:42:44 +0900

    Update 완주하지 못한 선수.java

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,6 +1,26 @@
+import java.util.*;
+
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
+        
+        int part_length = participant.length;
+        int compl_length = completion.length;
+        
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        
+        for(int i = 0; i < compl_length; i++){
+            if(!participant[i].equals(completion[i])){
+                answer = participant[i];
+                
+                break;
+            }
+        }
+        
+        if(answer.isEmpty()){
+            answer = participant[part_length-1];
+        }
         return answer;
     }
 }
